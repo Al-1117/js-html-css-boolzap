3e10b5d9d2ee05bd8bbd4286c20fc93de6b04456
Alioune <alioune1117@gmail.com>
2020-06-17 07:49:22 +0200

    aggiunto le conversazioni

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
 $(document).ready(
   function(){
-    // Core il messagio clone in modo da dupplicarlo e cambiare il testo.
-    var cloneMessage = $('.template .sent_message').clone();
-    console.log();
-    var newMessage = $(cloneMessage).find('span').text('Ciao');
-    $('.chat_container').append(newMessage);
+
+
+    $(document).on('click', '#send_button',
+      function(){
+        // Core il messagio clone in modo da dupplicarlo e cambiare il testo.
+        // Creo le variabili per eseguire l'operazione
+        var newMessage = $('.message_input input').val();
+        var cloneMessage = $('.template .sent_message').clone();
+
+      console.log(cloneMessage);
+      console.log(newMessage);
+      // Cambio il testo del messaggio con il nuovo messaggio immesso dall'utente
+      $(cloneMessage).find('span').text(newMessage);
+      // Appendo il messaggio alla chat
+      $('.chat_container').append(cloneMessage);
+
+
+
+
+    }
+    )
 
 
 
