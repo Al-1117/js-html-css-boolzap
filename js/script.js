73e6eac749a254f9d70442ddce32e0d79d56f4af
Alioune <alioune1117@gmail.com>
2020-06-22 12:59:47 +0200

    aggiunto il controllo per non inviare il messaggio se vuoto

--- js/script.js
+++ js/script.js
@@ -11,21 +11,12 @@ $(document).ready(
       // Setto il timeout di 2 secondi per il messagio da ricevere
       setTimeout(messageReceived, 2000);
 
-
-
-
       // SCROLL DEI NUOVI MESSAGGI IN AUTOMATICO ANCHE TRAMITE
-
       $('.chat_container').scrollTop($('.chat_container').prop('scrollHeight'));
-
-
-
     }
   );
 
-
-
-    // INVIO DEI MESSAGGI TRAMITE L'APPOSITO TASTO
+  // INVIO DEI MESSAGGI TRAMITE L'APPOSITO TASTO
 
   $('.message_input input').keyup(
     function(){
@@ -63,7 +54,6 @@ $(document).ready(
   });
 
   // COMPARSA E SCOMPARSA DELLA DROPDOWN DELLE OPZIONI DEL MESSAGGIO
-
   // Comparsa dell'icona freccia all'entrata del messaggio
   $(document).on('mouseenter', '.message_baloon',
     function(){
@@ -74,25 +64,19 @@ $(document).ready(
   );
 
   // Scomparsa dell'icona freccia all'entrata del messaggio
-
   $(document).on('mouseleave', '.message_baloon',
     function(){
       $(this).find('.arrow').hide();
     }
-
   );
 
   // MOSTRO E NASCONDO IL CONTENITORE DELLE EMOJI AL PASSAGGIO DEL MOUSE SULL'ICONA
-
   // Mostro il contenitore delle emoji al passaggio del mouse sulla relativa icone
   $(document).on('mouseenter', '.emoji',
     function(){
       $(this).children('.emoji_dropdown').slideDown(1000);
     }
-
   );
-
-
 
   // Nascondo il contenitore delle emoji al click sulla relativa icone
   $(document).on('click', '.emoji',
@@ -101,20 +85,17 @@ $(document).ready(
     }
   );
 
+  // ELIMINAZIONE DEI MESSAGGI GENERATI DINAMICAMENTE E NON
 
   // Evento click sull'icona freccia
   $(document).on('click', '.arrow',
     function(){
       // Seleziono la dropdown attuale
       var currentDropdown = $(this).siblings('.dropdown');
-
       // Nascondo le altre dropdown che non sono le correnti
       $('.dropdown').not(currentDropdown).addClass('hidden');
-
       // Rendo attiva la dropdown corrente
       currentDropdown.toggleClass('hidden');
-
-
     }
   );
 
@@ -122,12 +103,10 @@ $(document).ready(
   $(document).on('click', '.delete',
     function (){
       $(this).parentsUntil('.message_baloon').remove();
-
     }
   );
 
   //Associazione dei contatti con le relative chat
-
   $(document).on('click', '.single_chat',
     function (){
       // Seleziono l'elemento della lista contatti tramite l'attributo
@@ -142,12 +121,10 @@ $(document).ready(
       // Aggiungo la classe active alla chat corrente
       currentChat.addClass('active');
 
-      // Aggiungo la classe della chata selezionata solo dalla
+      // Aggiungo la classe della chat selezionata solo dalla
       // chat corrente rimuovendola prima alle altre chat
       $('.single_chat').removeClass('selected_chat');
-
       $(this).addClass('selected_chat');
-
 
       // Modifico il nome e l'immagine del contatto in alto sopra la chat
       // con quello corrente
@@ -173,49 +150,53 @@ $(document).ready(
     // Creo il messagio clone in modo da dupplicarlo e cambiare il testo.
     // Creo le variabili per eseguire l'operazione
     var newMessageSent = $('.message_input input').val();
-    var cloneMessageSent = $('.template .sent_message').clone();
-    console.log(cloneMessageSent);
-    console.log(newMessageSent);
-    // Cambio il testo del messaggio con il nuovo messaggio immesso dall'utente
-    $(cloneMessageSent).find('span').text(newMessageSent);
+    // Controllo che la lunghezza del valore nell'input non sia pari 0
+     // prima di procedere con la funzione di invio del messaggio
+    if (newMessageSent.length != 0) {
+      var cloneMessageSent = $('.template .sent_message').clone();
+      console.log(cloneMessageSent);
+      console.log(newMessageSent);
+      // Cambio il testo del messaggio con il nuovo messaggio immesso dall'utente
+      $(cloneMessageSent).find('span').text(newMessageSent);
 
-    // Agiungo l'ora corrente
-    var date = new Date();
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-    var timeNow = addZeroBefore(hour) +':'+ addZeroBefore(minutes);
+      // Agiungo l'ora corrente
+      var date = new Date();
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      var timeNow = addZeroBefore(hour) +':'+ addZeroBefore(minutes);
 
-    $(cloneMessageSent).find('.time').text(timeNow);
+      $(cloneMessageSent).find('.time').text(timeNow);
 
 
-    // Appendo il messaggio alla chat
-    $('.chat_history.active').append(cloneMessageSent);
+      // Appendo il messaggio alla chat
+      $('.chat_history.active').append(cloneMessageSent);
 
-    // Resetto l'input della chat
-    var newMessageSent = $('.message_input input').val('');
+      // Resetto l'input della chat
+      var newMessageSent = $('.message_input input').val('');
 
-    // Premendo il tasto per inviare il messaggio:
-    // l'eaeroplanino di carta viene nascosto
-    $('.plane').removeClass('active').addClass('hidden');
-    // E al suo posto comparirà il microfono come default
-    $('.mic').removeClass('hidden').addClass('active');
-    // Visualizzo lo stato ("Sta scrivendo...")
-    $('.typing').removeClass('hidden').addClass('active');
-    // Nascondo ultimo accesso
-    $('.last_seen').addClass('hidden');
+      // Premendo il tasto per inviare il messaggio:
+      // l'eaeroplanino di carta viene nascosto
+      $('.plane').removeClass('active').addClass('hidden');
+      // E al suo posto comparirà il microfono come default
+      $('.mic').removeClass('hidden').addClass('active');
+      // Visualizzo lo stato ("Sta scrivendo...")
+      $('.typing').removeClass('hidden').addClass('active');
+      // Nascondo ultimo accesso
+      $('.last_seen').addClass('hidden');
+
+
+    }
 
   }
 
   // MESSAGGI RICEVUTI
   function messageReceived(){
-
     // Clono il messaggio da ricevere dal template
     var cloneMessageReceived = $('.template .received_message').clone();
 
     console.log(cloneMessageReceived);
     //Cambio il testo del messaggio nel testo predefinito
     $(cloneMessageReceived).find('span').text('ok');
-
 
     // Agiungo l'ora corrente
     var date = new Date();
@@ -236,7 +217,6 @@ $(document).ready(
     // Aggiorno l'accesso in alto a sinistra
     $('.right_side').find('.last_seen_time').text(timeNow);
 
-
   }
 
   // Funzione per aggiungere lo '0' prima dell'ora / minuto
@@ -249,8 +229,4 @@ $(document).ready(
       return number;
     }
   }
-
-
-
-  }
-);
+});
